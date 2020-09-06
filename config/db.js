adc53d5ed17f73191b2a67e8a10635d017f7df56
Marco Meyer <marcomeyer@hispeed.ch>
2020-09-06 09:41:13 +0200

    finalize backend (shortcut; copy from course repo)

--- config/db.js
+++ config/db.js
@@ -6,13 +6,15 @@ const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
       useUnifiedTopology: true,
     });
 
-    console.log('MongoDB connected');
+    console.log('MongoDB Connected...');
   } catch (err) {
     console.error(err.message);
-
+    // Exit process with failure
     process.exit(1);
   }
 };
--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect Database
 connectDB();
 
+// Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API running'));
+app.get('/', (req, res) => res.send('API Running'));
 
+// Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
